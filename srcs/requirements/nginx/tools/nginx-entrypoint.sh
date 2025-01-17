b51317ccb403c79dfb5e3224301e0448a7cc9fb4
Anita Karabanowicz <akaraban@student.42wolfsburg.de>
2025-01-17 18:38:37 +0100

    inception form vogosphere

---
+++ srcs/requirements/nginx/tools/nginx-entrypoint.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Ensure DOMAIN_NAME is set
+if [ -z "$DOMAIN_NAME" ]; then
+    echo "Error: DOMAIN_NAME is not set."
+    exit 1
+fi
+
+# On the first container run, generate a certificate and configure the server
+if [ ! -e /etc/.firstrun ]; then
+    # Create the SSL directory if it doesn't exist
+    mkdir -p /etc/nginx/ssl
+
+    # Generate a certificate for HTTPS
+    openssl req -x509 -days 365 -newkey rsa:2048 -nodes \
+        -out '/etc/nginx/ssl/cert.crt' \
+        -keyout '/etc/nginx/ssl/cert.key' \
+        -subj "/CN=$DOMAIN_NAME" || { echo "SSL certificate generation failed"; exit 1; }
+
+    # Configure nginx to serve static WordPress files and pass PHP requests
+    cat << EOF >> /etc/nginx/http.d/default.conf
+server {
+    listen 80;
+    server_name $DOMAIN_NAME;
+    return 301 https://\$server_name\$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name $DOMAIN_NAME;
+
+    ssl_certificate /etc/nginx/ssl/cert.crt;
+    ssl_certificate_key /etc/nginx/ssl/cert.key;
+    ssl_protocols TLSv1.3;
+    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+
+    root /var/www/html;
+    index index.php index.html index.htm;
+
+    add_header X-Content-Type-Options nosniff;
+    add_header X-Frame-Options SAMEORIGIN;
+    add_header X-XSS-Protection "1; mode=block";
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
+
+    location ~ [^/]\.php(/|\$) {
+        try_files \$fastcgi_script_name =404;
+
+        fastcgi_pass wordpress:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param PATH_INFO \$fastcgi_path_info;
+        fastcgi_split_path_info ^(.+\.php)(/.*)\$;
+        include fastcgi_params;
+    }
+
+    location ~* \.(jpg|jpeg|png|gif|ico|css|js)$ {
+        expires 30d;
+        add_header Cache-Control "public, no-transform";
+    }
+}
+EOF
+    touch /etc/.firstrun
+fi
+
+# Start NGINX
+exec nginx -g 'daemon off;'
